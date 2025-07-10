00aa770ba75a74189d2717f5624a6db61739e379
sayalicse <spsayalipatil2003@gmail.com>
2025-07-10 13:45:18 +0530

    Home Page Work is Complete

--- lib/Screens/Home/homepage.dart
+++ lib/Screens/Home/homepage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:instagram_ui_series/Widgets/uihelper.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,6 +8,14 @@ class HomePage extends StatelessWidget {
       "name": "Sayali_Patil"
     },
     {
+      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnRGQuizczA_CYF0Lo-xuCE1FWvsO7ZdsGWw&s",
+      "name": "Satyajeetp"
+    },
+    {
+      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStJ9cnXUWrzDFunS7hwH7x4WivNEHPYTpMrA&s",
+      "name": "Satyap"
+    },
+    {
       "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNE_Q4uvf0d2194gczvijuY_w8OAiAPMNHWeD-x8HJuV1CMF2kg-g--upfSEtkgZ9N_xU&usqp=CAU",
       "name": "Sayali_Patil"
     },
@@ -25,20 +32,12 @@ class HomePage extends StatelessWidget {
       "name": "Sayali_Patil"
     },
     {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNE_Q4uvf0d2194gczvijuY_w8OAiAPMNHWeD-x8HJuV1CMF2kg-g--upfSEtkgZ9N_xU&usqp=CAU",
-      "name": "Sayali_Patil"
-    },
-    {
       "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnRGQuizczA_CYF0Lo-xuCE1FWvsO7ZdsGWw&s",
       "name": "Satyajeetp"
     },
     {
       "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStJ9cnXUWrzDFunS7hwH7x4WivNEHPYTpMrA&s",
       "name": "Satyap"
-    },
-    {
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNE_Q4uvf0d2194gczvijuY_w8OAiAPMNHWeD-x8HJuV1CMF2kg-g--upfSEtkgZ9N_xU&usqp=CAU",
-      "name": "Sayali_Patil"
     },
     {
       "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNE_Q4uvf0d2194gczvijuY_w8OAiAPMNHWeD-x8HJuV1CMF2kg-g--upfSEtkgZ9N_xU&usqp=CAU",
@@ -57,7 +56,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black, // Match Instagram dark theme
+      backgroundColor: Colors.black,
       appBar: AppBar(
         toolbarHeight: 88,
         backgroundColor: Colors.black12,
@@ -70,59 +69,375 @@ class HomePage extends StatelessWidget {
               icon: UiHelper.CustomImage(imgurl: 'igtv.png')),
           IconButton(
               onPressed: () {},
-              icon: UiHelper.CustomImage(imgurl: 'messanger.png'))
+              icon: UiHelper.CustomImage(imgurl: 'messanger.png')),
         ],
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 95,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: arrcontent.length,
-              padding: const EdgeInsets.symmetric(horizontal: 4),
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 2),
-                  child: Column(
-                    children: [
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundImage:
-                        NetworkImage(arrcontent[index]["img"].toString()),
-                      ),
-                      const SizedBox(height: 6),
-                      SizedBox(
-                        width: 65,
-                        child: Text(
-                          arrcontent[index]["name"].toString(),
-                          style:
-                          const TextStyle(fontSize: 12, color: Colors.white),
-                          overflow: TextOverflow.ellipsis,
-                          textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // 🔹 Horizontal Story List
+            SizedBox(
+              height: 95,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: arrcontent.length,
+                padding: const EdgeInsets.symmetric(horizontal: 4),
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 6),
+                    child: Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundImage: NetworkImage(
+                            arrcontent[index]["img"].toString(),
+                          ),
                         ),
-                      )
-                    ],
-                  ),
-                );
-              },
+                        const SizedBox(height: 6),
+                        SizedBox(
+                          width: 65,
+                          child: Text(
+                            arrcontent[index]["name"].toString(),
+                            style: const TextStyle(
+                                fontSize: 12, color: Colors.white),
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.center,
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
             ),
-          ),
-          Container(
-            height: 54,
-            width: 375,
-            color: Colors.black12,
-            child: ListTile(
-              leading:
-              CircleAvatar(
-                radius: 20,
-                backgroundColor: Colors.transparent,
-                child: UiHelper.CustomImage(imgurl: 'google.png'),
-              )
+
+            // 🔹 Post Header (User Info + More Icon)
+            Container(
+              color: Colors.black12,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: ListTile(
+                contentPadding: EdgeInsets.zero,
+                leading: CircleAvatar(
+                  radius: 20,
+                  backgroundImage: AssetImage('assets/images/instadp.png'),
+                ),
+                title: Text(
+                  'Sayali_Patil',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 13),
+                ),
+                subtitle: Text(
+                  'Takari, India',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 11),
+                ),
+                trailing: Image.asset(
+                  'assets/images/moreicon.png',
+                  height: 20,
+                  width: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+SizedBox(
+  height: 8,
+),
+            // 🔹 Post Image
+            Container(
+              clipBehavior: Clip.antiAlias,
+              width: double.infinity,
+              height: 400,
+              decoration: BoxDecoration(
+
+              ),
+              child: Image.asset(
+                'assets/images/rectangle.png',
+                fit: BoxFit.cover,
+              ),
 
             ),
-          )
-        ],
+            SizedBox(
+              height: 8,
+            ),
+            SizedBox(height: 8),
+
+// Post Action Buttons (Like, Comment, Share)
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+              color: Colors.black12,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        'assets/images/like.png',
+                        height: 28,
+                        width: 28,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: 16),
+                      Image.asset(
+                        'assets/images/comment.png',
+                        height: 28,
+                        width: 28,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: 16),
+                      Image.asset(
+                        'assets/images/messanger.png',
+                        height: 28,
+                        width: 28,
+                        color: Colors.white,
+                      ),
+                      Spacer(),
+                      Image.asset(
+                        'assets/images/save.png',
+                        height: 24,
+                        width: 24,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/images/oval.png',
+                        height: 24,
+                        width: 24,
+
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          style: TextStyle(fontSize: 15, color: Colors.white),
+                          children: [
+                            TextSpan(
+                              text: 'Liked by ',
+                              style: TextStyle(fontWeight: FontWeight.normal),
+                            ),
+                            TextSpan(
+                              text: 'Sayali_Patil',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            TextSpan(
+                              text: ' and ',
+                              style: TextStyle(fontWeight: FontWeight.normal),
+                            ),
+                            TextSpan(
+                              text: '44,686 others',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                      child:RichText(
+
+                          text:TextSpan(
+                              style: TextStyle(fontSize: 15, color: Colors.white),
+                              children: [
+                                TextSpan(
+                                  text: 'Sayali_Patil ',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                TextSpan(
+                                  text: ' The game in japan was amazing and i want to share some photos ',
+                                  style: TextStyle(fontWeight: FontWeight.normal),
+                                ),
+
+                              ]
+
+                          ) )
+                  )
+
+                ],
+              )
+
+
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Container(
+              color: Colors.black12,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: ListTile(
+                contentPadding: EdgeInsets.zero,
+                leading: CircleAvatar(
+                  radius: 20,
+                  backgroundImage: AssetImage('assets/images/instadp.png'),
+                ),
+                title: Text(
+                  'Satyajeet Patil',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 13),
+                ),
+                subtitle: Text(
+                  'Delhi, India',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 11),
+                ),
+                trailing: Image.asset(
+                  'assets/images/moreicon.png',
+                  height: 20,
+                  width: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Container(
+              clipBehavior: Clip.antiAlias,
+              width: double.infinity,
+              height: 400,
+              decoration: BoxDecoration(
+
+              ),
+              child: Image.asset(
+                'assets/images/rectangle.png',
+                fit: BoxFit.cover,
+              ),
+
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            SizedBox(height: 8),
+            Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                color: Colors.black12,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          'assets/images/like.png',
+                          height: 28,
+                          width: 28,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 16),
+                        Image.asset(
+                          'assets/images/comment.png',
+                          height: 28,
+                          width: 28,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 16),
+                        Image.asset(
+                          'assets/images/messanger.png',
+                          height: 28,
+                          width: 28,
+                          color: Colors.white,
+                        ),
+                        Spacer(),
+                        Image.asset(
+                          'assets/images/save.png',
+                          height: 24,
+                          width: 24,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Row(
+                      children: [
+                        Image.asset(
+                          'assets/images/oval.png',
+                          height: 24,
+                          width: 24,
+
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            style: TextStyle(fontSize: 15, color: Colors.white),
+                            children: [
+                              TextSpan(
+                                text: 'Liked by ',
+                                style: TextStyle(fontWeight: FontWeight.normal),
+                              ),
+                              TextSpan(
+                                text: 'Sayali_Patil',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              TextSpan(
+                                text: 'Satyajeet_Patil',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              TextSpan(
+                                text: ' and ',
+                                style: TextStyle(fontWeight: FontWeight.normal),
+                              ),
+                              TextSpan(
+                                text: '44,686 others',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+
+                      ],
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child:RichText(
+
+                            text:TextSpan(
+                                style: TextStyle(fontSize: 15, color: Colors.white),
+                                children: [
+                                  TextSpan(
+                                    text: 'Satyajeet_Patil ',
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  TextSpan(
+                                    text: ' The nature is beautiful and location is nice you can go and explore it ',
+                                    style: TextStyle(fontWeight: FontWeight.normal),
+                                  ),
+
+                                ]
+
+                            ) )
+                    )
+
+                  ],
+                )
+
+
+            ),
+
+
+
+
+          ],
+        ),
       ),
     );
   }
